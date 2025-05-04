5cf2267618c2390ff537e2b64a910814a5a7e906
stupienius <wangjay0401@gmail.com>
2025-05-04 16:10:55 +0800

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/chapter", async (req, res) => {
+  try {
+    const url =
+      "https://www.royalroad.com/fiction/21220/mother-of-learning/chapter/301778/1-good-morning-brother";
+    const { data } = await axios.get(url);
+    console.log(data);
+    const $ = cheerio.load(data);
+    const chapterText = $(".chapter-content").html();
+    console.log(chapterText);
+    res.send({ content: chapterText });
+  } catch (err) {
+    res.status(500).send({ error: "Failed to fetch chapter" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
